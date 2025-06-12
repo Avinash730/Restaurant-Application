d43dc79731f17fd55426d31018aa9720f14539d0
Avinash Rai <arai16006@gmail.com>
2025-06-12 14:30:19 +0530

    first commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">AVINASH RAI</div>
+          <div className="right">
+            <p>B43 Beta-1-Greater Noida Uttar Pradesh, India</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Avinash Rai</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By AvinashRai.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
